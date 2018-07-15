d617e17aa8c8cd7e4e2eee8f5662cb57d37cd704
nishinohi <tomoya0209@gmail.com>
2018-07-15 21:04:23 +0900

    リファクタリング

--- src/trapModule.cpp
+++ src/trapModule.cpp
@@ -43,60 +43,6 @@ void TrapModule::setupTask() {
 }
 
 /********************************************
- * Camera メソッド
- *******************************************/
-/**
- * カメラスナップショット要求
- * TODO: タスク完了後の bool 値を判定に使用したい
- */
-bool TrapModule::snapCamera(int resolution) {
-    DEBUG_MSG_LN("snapCamera");
-    if (!_config._cameraEnable) {
-        DEBUG_MSG_LN("camera cannot use");
-        return false;
-    }
-    // 画像転送タスク実行中はカメラ撮影は実行しない
-    if (_sendPictureTask.isEnabled()) {
-        DEBUG_MSG_LN("camera cannot use because picture send task is running");
-        return false;
-    }
-    _camera.setResolution(resolution);
-    // タスク作成前の場合はタスクを作成
-    if (strncmp(pcTaskGetTaskName(_taskHandle[0]), CAMERA_TASK_NAME, strlen(CAMERA_TASK_NAME)) !=
-        0) {
-        xTaskCreatePinnedToCore(TrapModule::snapCameraTask, CAMERA_TASK_NAME, TASK_MEMORY, this, 2,
-                                &_taskHandle[0], 0);
-        return true;
-    } else {
-        if (eTaskGetState(_taskHandle[0]) == eSuspended) {
-            vTaskResume(_taskHandle[0]);
-            return true;
-        }
-    }
-    return false;
-}
-
-/**
- * カメラ撮影タスク
- */
-void TrapModule::snapCameraTask(void *arg) {
-    TrapModule *trapModule = reinterpret_cast<TrapModule *>(arg);
-    while (1) {
-        if (trapModule->_camera.saveCameraData()) {
-            DEBUG_MSG_LN("snap success");
-            // メッセージ送信タスク実行中でなければ送信タスク開始
-            trapModule->_sendPictureTask.setIterations(5);
-            trapModule->_sendPictureTask.enable();
-        } else {
-            DEBUG_MSG_LN("snap failed");
-        }
-        DEBUG_MSG_LN("camera task suspend");
-        vTaskSuspend(trapModule->_taskHandle[0]);
-        TASK_DELAY(1);
-    }
-}
-
-/********************************************
  * loop メソッド
  *******************************************/
 /**
@@ -603,6 +549,60 @@ void TrapModule::moduleCheckStart() {
     if (!_config._isBatteryDead && !_checkBatteryTask.isEnabled()) {
         DEBUG_MSG_LN("Start Battery Check");
         _checkBatteryTask.enableDelayed(BATTERY_CHECK_DELAYED);
+    }
+}
+
+/********************************************
+ * Camera メソッド
+ *******************************************/
+/**
+ * カメラスナップショット要求
+ * TODO: タスク完了後の bool 値を判定に使用したい
+ */
+bool TrapModule::snapCamera(int resolution) {
+    DEBUG_MSG_LN("snapCamera");
+    if (!_config._cameraEnable) {
+        DEBUG_MSG_LN("camera cannot use");
+        return false;
+    }
+    // 画像転送タスク実行中はカメラ撮影は実行しない
+    if (_sendPictureTask.isEnabled()) {
+        DEBUG_MSG_LN("camera cannot use because picture send task is running");
+        return false;
+    }
+    _camera.setResolution(resolution);
+    // タスク作成前の場合はタスクを作成
+    if (strncmp(pcTaskGetTaskName(_taskHandle[0]), CAMERA_TASK_NAME, strlen(CAMERA_TASK_NAME)) !=
+        0) {
+        xTaskCreatePinnedToCore(TrapModule::snapCameraTask, CAMERA_TASK_NAME, TASK_MEMORY, this, 2,
+                                &_taskHandle[0], 0);
+        return true;
+    } else {
+        if (eTaskGetState(_taskHandle[0]) == eSuspended) {
+            vTaskResume(_taskHandle[0]);
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * カメラ撮影タスク
+ */
+void TrapModule::snapCameraTask(void *arg) {
+    TrapModule *trapModule = reinterpret_cast<TrapModule *>(arg);
+    while (1) {
+        if (trapModule->_camera.saveCameraData()) {
+            DEBUG_MSG_LN("snap success");
+            // メッセージ送信タスク実行中でなければ送信タスク開始
+            trapModule->_sendPictureTask.setIterations(5);
+            trapModule->_sendPictureTask.enable();
+        } else {
+            DEBUG_MSG_LN("snap failed");
+        }
+        DEBUG_MSG_LN("camera task suspend");
+        vTaskSuspend(trapModule->_taskHandle[0]);
+        TASK_DELAY(1);
     }
 }
 
