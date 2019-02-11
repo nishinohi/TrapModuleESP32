1e45664db83c3b42c05ea6523180f9ac63d02e5d
nishinohi <tomoya0209@gmail.com>
2019-02-11 22:27:09 +0900

    稼働限界時間を稼働開始時刻と現在時刻の比較ではなく、起動後からの経過時間でチェックする。

--- src/trapModule.cpp
+++ src/trapModule.cpp
@@ -72,7 +72,6 @@ void TrapModule::update() {
     }
     // DeepSleep 開始
     if (_config._isSleep) {
-        DEBUG_MSG_LN("deep sleep start.");
         delay(SYNC_SLEEP_INTERVAL);
         shiftDeepSleep();
     }
@@ -81,7 +80,7 @@ void TrapModule::update() {
         return;
     }
     // 稼働時間超過により強制 DeepSleep
-    if (now() - _config._wakeTime > _config._workTime) {
+    if (millis() > _config._workTime * 1000) {
         DEBUG_MSG_LN("work time limit.");
         _config._isSleep = true;
     }
@@ -434,7 +433,7 @@ void TrapModule::startSendModuleState() {
     if (_config._isSendModuleState) {
         return;
     }
-    taskStart(_sendModuleStateTask);
+    taskStart(_sendModuleStateTask, DEF_INTERVAL);
 }
 
 /********************************************
